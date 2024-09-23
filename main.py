d94ab465018ac2acaa1ed08d356c5b3e8adc09cf
Lek29 <ligioner29@mail.ru>
2024-09-23 17:04:17 +0800

    добавление main.py

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import urwid
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def has_digit(password):
+    return any([sign.isdigit() for sign in password])
 
 
-# Press the green button in the gutter to run the script.
+def has_letters(password):
+    return any([sign.isalpha() for sign in password])
+
+
+def is_very_long(password):
+    return len(password) > 12
+
+
+def has_upper_letters(password):
+    return any([sign.isupper() for sign in password])
+
+
+def has_lower_letters(password):
+    return any([sign.islower() for sign in password])
+
+
+def has_symbols(password):
+    return any([sign in '%#' for sign in password])
+
+
+check_function = [
+    has_digit,
+    has_letters,
+    has_upper_letters,
+    has_lower_letters,
+    is_very_long,
+    has_symbols,
+]
+
+
+def calculate_rating(password):
+    score = 0
+    for func in check_function:
+        if func(password):
+            score += 2
+    return f'Рейтинг пароля: {score}'
+
+
+def on_password_change(edit, new_edit_text):
+    rating = calculate_rating(new_edit_text)
+    reply.set_text(f'{rating}')
+
+
+def main():
+    global reply
+    input_password = urwid.Edit('Введите пароль:')
+    reply = urwid.Text('Рейтинг пароля: 0')
+    menu = urwid.Pile([input_password, reply])
+    menu = urwid.Filler(menu, valign='middle')
+
+    urwid.connect_signal(input_password, 'change', on_password_change)
+    urwid.MainLoop(menu).run()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
